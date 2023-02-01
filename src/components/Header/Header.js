b2dc6ccf523dc4206599cc28c6f70bb732170874
Sofrosine <soultanmuh@gmail.com>
2023-02-01 14:16:28 +0700

    revamp sidebar and header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,103 @@
-import './Header.scss';
-import logo from '../../assets/images/logo.svg';
-import aniLogoWhite from '../../assets/images/ani-logo-white.png';
-import avatar from '../../assets/images/avatar.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import { history } from '../../store';
-import { ACTOR } from '../../constants/component.constant';
-import SubmitButton from '../elements/SubmitButton/SubmitButton';
-import { HOST } from '../../services/api';
+import "./Header.scss";
+import logo from "../../assets/images/logo.svg";
+import aniLogoWhite from "../../assets/images/ani-logo-white.png";
+import avatar from "../../assets/images/avatar.svg";
+import iconMore from "../../assets/icons/circle-chevron-down.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { useMemo, useState } from "react";
+import { history } from "../../store";
+import { ACTOR } from "../../constants/component.constant";
+import SubmitButton from "../elements/SubmitButton/SubmitButton";
+import { HOST } from "../../services/api";
 
-export default (props) => {
-    const [menuVisible, setMenuVisible] = useState(false);
-    const profilePhoto = props.actor && props.actor.photo ? `${HOST}/api/files/${props.actor.photo}` : avatar;
-    const profileName = props.actor && props.actor.name ? props.actor.name : 'User';
-    const actorPubKey = props.actor ? props.type === ACTOR.ADMIN ? props.actor.public_key : props.actor.public_key : undefined;
+const Header = (props) => {
+  const [menuVisible, setMenuVisible] = useState(false);
 
-    return (
-        <>
-            <div className="header">
-                <div className="header-left">
-                    <img src={aniLogoWhite}></img>
-                </div>
-                <div className="header-right">
-                    {props.actor ?
-                        <div className="profile">
-                            <img className='' src={profilePhoto} onClick={() => setMenuVisible(!menuVisible)} />
-                            <div>
-                                <span className="profile-name">{profileName}</span>
-                                <div className="profile-name">{profileName}</div>
-                            </div>
+  const profilePhoto = useMemo(() => {
+    return props.actor && props.actor.photo
+      ? `${HOST}/api/files/${props.actor.photo}`
+      : avatar;
+  }, [props.actor]);
 
-                        </div> : <></>
-                    }
-                    {menuVisible ?
-                        <div className="profile-options">
-                            <div className="menu">
-                                <div className="sub-menu" onClick={() => { if (props.logout) { history.push(`/profile?actor_type=${props.type}&actor_public_key=${actorPubKey}`) } }}>
-                                    <span>Details</span><FontAwesomeIcon icon={faSignOutAlt} />
-                                </div>
-                                <div className="sub-menu" onClick={() => { if (props.logout) { props.logout() } }}>
-                                    <span>Logout</span><FontAwesomeIcon icon={faSignOutAlt} />
-                                </div>
-                            </div>
-                        </div> : <div></div>
-                    }
-                </div>
+  const profileName = useMemo(() => {
+    return props.actor && props.actor.name ? props.actor.name : "User";
+  }, [props.actor]);
+
+  const profileRole = useMemo(() => {
+    return props.actor && props.actor.admin_role
+      ? props.actor.admin_role
+      : "User";
+  }, [props.actor]);
+
+  const actorPubKey = useMemo(() => {
+    return props.actor
+      ? props.type === ACTOR.ADMIN
+        ? props.actor.public_key
+        : props.actor.public_key
+      : undefined;
+  }, [props.actor]);
+
+  return (
+    <>
+      <div className="header">
+        <div className="header-left">
+          <img alt="logo-white" src={aniLogoWhite}></img>
+        </div>
+        <div className="header-right">
+          {props.actor ? (
+            <div className="profile">
+              <img className="avatar" src={profilePhoto} alt="profile" />
+              <div className="right">
+                <span className="name">{profileName}</span>
+                <div className="role">{profileRole}</div>
+              </div>
+              <img
+                className="more"
+                src={iconMore}
+                onClick={() => setMenuVisible(!menuVisible)}
+                alt="more"
+              />
             </div>
-        </>
-    );
-}
+          ) : (
+            <></>
+          )}
+          {menuVisible ? (
+            <div className="profile-options">
+              <div className="menu">
+                <div
+                  className="sub-menu"
+                  onClick={() => {
+                    if (props.logout) {
+                      history.push(
+                        `/profile?actor_type=${props.type}&actor_public_key=${actorPubKey}`
+                      );
+                    }
+                  }}
+                >
+                  <span>Details</span>
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </div>
+                <div
+                  className="sub-menu"
+                  onClick={() => {
+                    if (props.logout) {
+                      props.logout();
+                    }
+                  }}
+                >
+                  <span>Logout</span>
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,19 @@
 import "./Sidebar.scss";
 import menuIcon from "../../assets/icons/menu.svg";
+import homeActiveIcon from "../../assets/icons/home-active.svg";
+import homeInactiveIcon from "../../assets/icons/home-inactive.svg";
+import folderActiveIcon from "../../assets/icons/folder-active.svg";
+import folderInactiveIcon from "../../assets/icons/folder-inactive.svg";
+
 import { history } from "../../store";
 import { useEffect, useState } from "react";
 import React from "react";
 import { ACTOR } from "../../constants/component.constant";
 import { useSelector } from "react-redux";
 
-export default (props) => {
+const Sidebar = (props) => {
   const [isManageUser, setIsManageUser] = useState(false);
-  const admin = useSelector(state => state.getIn(['actor', 'admin']).toJS());
+  const admin = useSelector((state) => state.getIn(["actor", "admin"]).toJS());
 
   useEffect(() => {
     if (
@@ -27,18 +32,45 @@ export default (props) => {
     return (
       <React.Fragment>
         <div
-          className={!props.menu ? "menu active" : "menu"}
+          className={(!props.menu ? "menu active" : "menu") + " separator-menu"}
           onClick={() => history.push(`/dashboard/${props.actor}`)}
         >
-          <img src={menuIcon} />
-          <span>Dashboard</span>
+          <div className="iconName">
+            <img
+              alt="ic-home"
+              src={!props.menu ? homeActiveIcon : homeInactiveIcon}
+            />
+            <span>Dashboard</span>
+            {!props.menu ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
         </div>
         <div
-          className={props.menu === "manage_user" ? "menu active" : "menu"}
+          className={
+            (props.menu === "manage_user" ? "menu active" : "menu") +
+            " separator-sub-menu"
+          }
           onClick={() => setIsManageUser(!isManageUser)}
         >
-          <img src={menuIcon} />
-          <span>Manage Admin</span>
+          <div className="iconName">
+            <img
+              alt="ic-manage"
+              src={
+                props.menu === "manage_user"
+                  ? folderActiveIcon
+                  : folderInactiveIcon
+              }
+            />
+            <span>Manage Admin</span>
+            {props.menu === "manage-user" ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
         </div>
         {isManageUser ? (
           <React.Fragment>
@@ -48,19 +80,37 @@ export default (props) => {
                   ? "menu sub active"
                   : "menu sub"
               }
-              onClick={() => history.push(`/dashboard/${props.actor}?menu=institution-master`)}
+              onClick={() =>
+                history.push(
+                  `/dashboard/${props.actor}?menu=institution-master`
+                )
+              }
             >
-              <img src={menuIcon} />
-              <span>Institution Master</span>
+              <div className="iconName">
+                <span>Institution Master</span>
+                {props.menu === "institution-master" ? (
+                  <div className="dotActive" />
+                ) : (
+                  <div className="dotInactive" />
+                )}
+              </div>
             </div>
             <div
               className={
                 props.menu === "admin-master" ? "menu sub active" : "menu sub"
               }
-              onClick={() => history.push(`/dashboard/${props.actor}?menu=admin-master`)}
+              onClick={() =>
+                history.push(`/dashboard/${props.actor}?menu=admin-master`)
+              }
             >
-              <img src={menuIcon} />
-              <span>Admin Master</span>
+              <div className="iconName">
+                <span>Admin Master</span>
+                {props.menu === "admin-master" ? (
+                  <div className="dotActive" />
+                ) : (
+                  <div className="dotInactive" />
+                )}
+              </div>
             </div>
           </React.Fragment>
         ) : (
@@ -68,79 +118,128 @@ export default (props) => {
         )}
       </React.Fragment>
     );
-  }
+  };
 
   const institutionAdminMenu = () => {
     return (
       <React.Fragment>
         <div
-          className={!props.menu ? "menu active" : "menu"}
+          className={(!props.menu ? "menu active" : "menu") + " separator-menu"}
           onClick={() => history.push(`/dashboard/${props.actor}`)}
         >
-          <img src={menuIcon} />
-          <span>Dashboard</span>
+          <div className="iconName">
+            <img
+              alt="ic-home"
+              src={!props.menu ? homeActiveIcon : homeInactiveIcon}
+            />
+            <span>Dashboard</span>
+            {!props.menu ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
         </div>
         <div
-          className={props.menu === "manage-certificate" ? "menu active" : "menu"}
-          onClick={() => history.push(`/dashboard/${props.actor}?menu=manage-certificate`)}
-        >
-          <img src={menuIcon} />
-          <span>Manage Certificates</span>
-        </div>
-        {/* <div
           className={
-            props.menu === "manage_user" ? "menu active" : "menu"
+            props.menu === "manage-certificate" ? "menu active" : "menu"
           }
-          onClick={() => history.push(`/dashboard/${props.actor}?menu=user-master`)}
+          onClick={() =>
+            history.push(`/dashboard/${props.actor}?menu=manage-certificate`)
+          }
         >
-          <img src={menuIcon} />
-          <span>Manage User</span>
-        </div> */}
+          <div className="iconName">
+            <img
+              alt="ic-home"
+              src={
+                props.menu === "manage-certificate"
+                  ? folderActiveIcon
+                  : folderInactiveIcon
+              }
+            />
+            <span>Manage Certificates</span>
+
+            {props.menu === "manage-certificate" ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     );
-  }
+  };
 
   const adminMenu = () => {
-    if (admin && admin.admin_role === 'MAIN') {
+    if (admin && admin.admin_role === "MAIN") {
       return mainAdminMenu();
     }
 
     return institutionAdminMenu();
-  }
+  };
 
   const userMenu = () => {
     return (
       <React.Fragment>
         <div
-          className={!props.menu ? "menu active" : "menu"}
+          className={(!props.menu ? "menu active" : "menu") + " separator-menu"}
           onClick={() => history.push(`/dashboard`)}
         >
-          <img src={menuIcon} />
-          <span>Dashboard</span>
+          <div className="iconName">
+            <img
+              alt="ic-home"
+              src={!props.menu ? homeActiveIcon : homeInactiveIcon}
+            />
+            <span>Dashboard</span>
+            {!props.menu ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
         </div>
         <div
-          className={props.menu === "manage-certificate" ? "menu active" : "menu"}
-          onClick={() => history.push("/dashboard/USER?menu=manage-certificate")}
+          className={
+            props.menu === "manage-certificate" ? "menu active" : "menu"
+          }
+          onClick={() =>
+            history.push("/dashboard/USER?menu=manage-certificate")
+          }
         >
-          <img src={menuIcon} />
-          <span>My Certificates</span>
+          <div className="iconName">
+            <img
+              alt="ic-home"
+              src={
+                props.menu === "manage-certificate"
+                  ? folderActiveIcon
+                  : folderInactiveIcon
+              }
+            />
+            <span>Manage Certificates</span>
+
+            {props.menu === "manage-certificate" ? (
+              <div className="dotActive" />
+            ) : (
+              <div className="dotInactive" />
+            )}
+          </div>
         </div>
       </React.Fragment>
     );
-  }
+  };
 
   const resolveMenu = () => {
-    switch(props.actor) {
+    switch (props.actor) {
       case ACTOR.ADMIN:
         return adminMenu();
       case ACTOR.USER:
         return userMenu();
+      default:
+        return <div />;
     }
-  }
+  };
 
-  return (
-    <div className="sidebar">
-      {resolveMenu()}
-    </div>
-  );
+  return <div className="sidebar">{resolveMenu()}</div>;
 };
+
+export default Sidebar;
